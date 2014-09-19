c268a27e75643bc12bfeb10837a2412670d6c6e1
Gang Jin <smartwsw@gmail.com>
2014-09-19 21:16:23 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  21:16:23 up 23:46,  5 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-4*[getty]      |-login---bash      |-login---bash---bash---less      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash-+-make---sh---nemu      |                           `-make---sh-+-git      |                                       `-pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -65,6 +65,7 @@ static void cmd_r() {
 		char c;
 		while(1) {
 			printf("The program is already running. Restart the program? (y or n)");
+			fflush(stdout);
 			scanf("%c", &c);
 			switch(c) {
 				case 'y': goto restart_;
