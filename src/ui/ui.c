9b8e4d72e6d1cb95173f222a285ba40d96296273
Gang Jin <smartwsw@gmail.com>
2014-09-19 22:18:05 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.57-3 i686 GNU/Linux  22:18:05 up 1 day, 48 min,  5 users,  load average: 0.09, 0.04, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-4*[getty]      |-login---bash      |-login---bash---bash---less      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash-+-make---sh---nemu      |                           `-make---sh-+-git      |                                       `-pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -66,7 +66,7 @@ static void cmd_r() {
 		while(1) {
 			printf("The program is already running. Restart the program? (y or n)");
 			fflush(stdout);
-			scanf("%c", &c);
+			scanf(" %c", &c);
 			switch(c) {
 				case 'y': goto restart_;
 				case 'n': return;
@@ -117,7 +117,24 @@ void main_loop() {
 			}
 				printf("%d\n",para);
 			cmd_si(para);
+		}
+		else if(strcmp(p, "info") == 0) {
+			p = strtok(NULL," ");
+			if (p == NULL)
+				printf("Invalid parameter!");
+			else {
+				if (strcmp(p, "r") == 0) {
+					printf("EAX      %p     %d/n",&cpu.eax,cpu.eax);
+					printf("ECX      %p     %d/n",&cpu.ecx,cpu.ecx);
+					printf("EDX      %p     %d/n",&cpu.edx,cpu.edx);
+					printf("EBX      %p     %d/n",&cpu.ebx,cpu.ebx);
+					printf("ESP      %p     %d/n",&cpu.esp,cpu.esp);
+					printf("EBP      %p     %d/n",&cpu.ebp,cpu.ebp);
+					printf("ESI      %p     %d/n",&cpu.esi,cpu.esi);
+					printf("EDI      %p     %d/n",&cpu.edi,cpu.edi);
+				}
 			}
+		}
 		/* TODO: Add more commands */
 
 		else { printf("Unknown command '%s'\n", p); }
