77992939778a51dad8fe6b4ef4dc234fdb7b23a5
Gang Jin <smartwsw@gmail.com>
2014-10-06 01:53:14 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  01:53:14 up 1 day, 11:12,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash      |-2*[sshd---sshd---bash---vi]      |-sshd-+-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -292,6 +292,7 @@ uint32_t expr(char *e, bool *success) {
 		if (tokens[i].type=='*') {
 			if (tokens[i-1].type!=NUM||tokens[i-1].type!=HEX)
 				tokens[i].type=LEA;
+			printf("loc %d changed\n",i);
 		}
 	}
 	/* TODO: Implement code to evaluate the expression. */
