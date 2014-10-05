1a9404e67fb0144a4fe84af779b040b4721d9798
Gang Jin <smartwsw@gmail.com>
2014-10-06 01:51:55 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  01:51:55 up 1 day, 11:11,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash      |-2*[sshd---sshd---bash---vi]      |-sshd-+-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -287,6 +287,13 @@ uint32_t expr(char *e, bool *success) {
 		return 0;
 	}
 
+	int i;
+	for (i=0;i<nr_token;i++) {
+		if (tokens[i].type=='*') {
+			if (tokens[i-1].type!=NUM||tokens[i-1].type!=HEX)
+				tokens[i].type=LEA;
+		}
+	}
 	/* TODO: Implement code to evaluate the expression. */
 	int tmp=0;
 	for (;tmp<nr_token;tmp++) {
