220038919fec233df6a10c8d6e076255601b6894
Gang Jin <smartwsw@gmail.com>
2014-10-06 10:10:37 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  10:10:37 up 1 day, 12:15,  3 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---vi      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -290,7 +290,7 @@ uint32_t expr(char *e, bool *success) {
 	}
 	for (i=0;i<nr_token;i++) {
 		if (tokens[i].type=='*') {
-			if (tokens[i-1].type!=NUM&&tokens[i-1].type!=HEX)
+			if (tokens[i-1].type!=NUM&&tokens[i-1].type!=HEX&&tokens[i-1].type!=REG)
 				tokens[i].type=LEA;
 		}
 	}
