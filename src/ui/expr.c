f9c6ddac89e4b9e9ef6d5a636d3322166bfe48ec
Gang Jin <smartwsw@gmail.com>
2014-10-06 09:42:52 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  09:42:52 up 1 day, 11:48,  5 users,  load average: 0.05, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash      |-sshd---sshd---bash---vi      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---vi      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -286,7 +286,7 @@ uint32_t eval(int p,int q,bool *success) {
 uint32_t expr(char *e, bool *success) {
 	int i;
 	for (i=0;i<32;i++) {
-		strcpy(tokens[i].str,"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
+		//strcpy(tokens[i].str,"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
 		printf("%s\n",tokens[i].str);
 	}
 	if(!make_token(e)) {
