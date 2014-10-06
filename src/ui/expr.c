53d3f220172e4a568699a9368a44a4a2c7afc03c
Gang Jin <smartwsw@gmail.com>
2014-10-06 09:39:29 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  09:39:29 up 1 day, 11:44,  5 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-dhclient      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash      |-sshd---sshd---bash---vi      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---vi      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -285,7 +285,7 @@ uint32_t eval(int p,int q,bool *success) {
 }
 uint32_t expr(char *e, bool *success) {
 	int i;
-	for (i=0;i<nr_token;i++)
+	for (i=0;i<32;i++)
 		strcpy(tokens[i].str,"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
 	if(!make_token(e)) {
 		*success = false;
