c3a1a2425095a32e96f9f0b0f23af763bf2d2cf1
Gang Jin <smartwsw@gmail.com>
2014-10-19 17:33:01 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  17:33:01 up 15:58,  5 users,  load average: 0.08, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash      |-udevd---2*[udevd]      |-udisks-daemon-+-udisks-daemon      |               `-{udisks-daemon}      |-upowerd---2*[{upowerd}]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/data-mov/push.c
+++ src/exec/data-mov/push.c
@@ -4,8 +4,9 @@ extern char suffix;
 
 make_helper(push_r_l) {
 	int reg_code = instr_fetch(eip,1) & 0x7;
+	int value = reg_l(reg_code);
 	cpu.esp = cpu.esp - 0x4;
-	swaddr_write(cpu.esp,4,reg_l(reg_code));
+	swaddr_write(cpu.esp,4,value);
 	
 	print_asm("push %%%s", regsl[reg_code]);
 	return 1;
