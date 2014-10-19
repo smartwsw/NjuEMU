45793360f04134c93688171571a77195b2caf39b
Gang Jin <smartwsw@gmail.com>
2014-10-19 16:00:26 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.60-1+deb7u3 i686 GNU/Linux  16:00:26 up 14:25,  5 users,  load average: 0.08, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-udisks-daemon-+-udisks-daemon      |               `-{udisks-daemon}      |-upowerd---2*[{upowerd}]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/data-mov/push.c
+++ src/exec/data-mov/push.c
@@ -1,7 +1,30 @@
 #include "exec/helper.h"
-make_helper(push_r_v) {
-	int opcode=swaddr_read(eip,1);
-	int reg_type=opcode&7;
-	printf("%d\n",reg_l(reg_type));
+#include "cpu/modrm.h"
+extern char suffix;
+
+make_helper(push_r_l) {
+	int reg_code = instr_fetch(eip,1) & 0x7;
+	cpu.esp = cpu.esp - 4;
+	swaddr_write(cpu.esp,4,reg_l(reg_code));
+	
+	print_asm("push $%%%s", regsl[reg_code]);
 	return 1;
+}
+make_helper(push_r_w) {
+	assert(0);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+make_helper(push_r_v) {
+	return (suffix == 'l' ? push_r_l(eip) : push_r_w(eip));
 }
