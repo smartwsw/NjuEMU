47ca750e82a437f6f7130ead8af447760daf2300
Gang Jin <smartwsw@gmail.com>
2014-11-02 21:14:22 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  21:14:22 up 1 day,  8:42,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-3*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/bin-ins/cmp-template.h
+++ src/exec/bin-ins/cmp-template.h
@@ -7,47 +7,57 @@ make_helper(concat(cmp_rm_i_, SUFFIX)) {
 	int opcode = instr_fetch(eip, 1);
 	m.val = instr_fetch(eip + 1,1);
 	int len = 1, result;
+	int imm;
+	swaddr_t addr;
+	if (opcode == 0x83) {
+		imm = instr_fetch(eip + 2,1);
+		imm = (imm << 24) >> 24;
+		len += 2;
+	}
+	else {
+		imm = instr_fetch(eip + 2, DATA_BYTE);
+		len += DATA_BYTE + 1;
+	}
+	if (m.mod == 3) {
+		int reg_value = REG(m.reg);
+		if (reg_value < imm) 
+			cpu.CF = 1;
+		result = reg_value - imm;
+	}
+	else {
+		len += read_ModR_M(eip + 1, &addr);
+		int m_val = swaddr_read(addr, 1);
+		result = m_val - imm;
+	}
 	switch (m.opcode) {
 		case 7 : {
-					 int imm;
-					 if (opcode == 0x83) {
-						 imm = instr_fetch(eip + 2,1);
-						 imm = (imm << 24) >> 24;
-						 len += 2;
-					 }
-					 else {
-						 imm = instr_fetch(eip + 2, DATA_BYTE);
-						 len += DATA_BYTE + 1;
-					 }
+					 if (m.mod == 3)
+						 print_asm("cmp"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
+					 else
+						 print_asm("cmp"str(SUFFIX)"\t\t$0x%x,0x%x",imm, addr);
+				 }
+		case 5 : {
 					 if (m.mod == 3) {
-						 int reg_value = REG(m.reg);
-						 if (reg_value < imm) 
-							 cpu.CF = 1;
-						 result = reg_value - imm;
+						 REG(m.reg) = result;
 						 print_asm("cmp"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
 					 }
 					 else {
-						 swaddr_t addr;
-						 len += read_ModR_M(eip + 1, &addr);
-						 int m_val = swaddr_read(addr, 1);
-						 result = m_val - imm;
+						 swaddr_write(addr, DATA_BYTE, result);
 						 print_asm("cmp"str(SUFFIX)"\t\t$0x%x,0x%x",imm, addr);
 					 }
-					 cpu.AF = 0;
-					 cpu.ZF = !!result;
-					 cpu.SF = (result >> 31) & 0x1;
-					 cpu.OF = 0;
-					 bool parity = 1;
-					 int i;
-					 for (i = 0;i < 8;i++) 
-						 if (((result >> i) & 0x1) == 1)
-							 parity = ~parity;
-					 cpu.PF = parity;
 				 }
-				 return len;
-		default :
-				 assert(0);
+				 cpu.AF = 0;
+				 cpu.ZF = !!result;
+				 cpu.SF = (result >> 31) & 0x1;
+				 cpu.OF = 0;
+				 bool parity = 1;
+				 int i;
+				 for (i = 0;i < 8;i++) 
+					 if (((result >> i) & 0x1) == 1)
+						 parity = ~parity;
+				 cpu.PF = parity;
 	}
+	return len;
 }
 make_helper(concat(cmp_a_i_,SUFFIX)) {
 	int imm = instr_fetch(eip + 1, DATA_BYTE);
