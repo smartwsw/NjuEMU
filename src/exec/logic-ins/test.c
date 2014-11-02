647634ddd6e7b49c57cf18186d5122b2c7f96224
Gang Jin <smartwsw@gmail.com>
2014-11-02 10:53:17 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  10:53:17 up 22:55,  8 users,  load average: 0.25, 0.07, 0.06 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-2*[sshd---sshd---bash---vi]      |-sshd---sshd---bash---make---sh-+-nemu      |                                `-tee      |-sshd-+-3*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/logic-ins/test.c
+++ src/exec/logic-ins/test.c
@@ -5,56 +5,92 @@ extern char suffix;
 make_helper(test_rm_r_l) {
 	ModR_M modrm;
 	modrm.val = instr_fetch(eip+1,1);
-	switch (modrm.mod) {
-		case 3: {
-					int right_code = modrm.R_M;
-					int left_code = modrm.reg;
-					int result = reg_l(left_code) & reg_l(right_code);
-					cpu.OF = 0;
-					cpu.CF = 0;
-					cpu.SF = (result >> 31) & 0x1;
-					cpu.ZF = !!result;
-					bool parity = 1;
-					int i;
-					for (i = 0;i < 8;i++) 
-						if (((result >> i) & 0x1) == 1)
-							parity = ~parity;
-					cpu.PF = parity;
-
-					print_asm("test %%%s,%%%s", regsl[left_code] , regsl[right_code]);
-					return 2;
-				}
-		default:
-				assert (0);
-				return 1;
+	int result;
+	if (modrm.mod == 3) {
+		int right_code = modrm.R_M;
+		int left_code = modrm.reg;
+		result = reg_l(left_code) & reg_l(right_code);
+		print_asm("test %%%s,%%%s", regsl[left_code] , regsl[right_code]);
 	}
+	else {
+		swaddr_t addr;
+		read_ModR_M(eip + 1, &addr);
+		int left_code = modrm.reg;
+		int right = swaddr_read(addr, 4);
+		result = right & reg_l(left_code);
+		print_asm("test %%%s,0x%x", regsl[left_code] , addr);
+	}
+	cpu.OF = 0;
+	cpu.CF = 0;
+	cpu.SF = (result >> 31) & 0x1;
+	cpu.ZF = !!result;
+	bool parity = 1;
+	int i;
+	for (i = 0;i < 8;i++) 
+		if (((result >> i) & 0x1) == 1)
+			parity = ~parity;
+	cpu.PF = parity;
+	return 2;
 }
 make_helper(test_rm_r_w) {
 	ModR_M modrm;
 	modrm.val = instr_fetch(eip+1,1);
-	switch (modrm.mod) {
-		case 3: {
-					int right_code = modrm.R_M;
-					int left_code = modrm.reg;
-					int result = reg_w(left_code) & reg_w(right_code);
-					cpu.OF = 0;
-					cpu.CF = 0;
-					cpu.SF = (result >> 31) & 0x1;
-					cpu.ZF = !!result;
-					bool parity = 1;
-					int i;
-					for (i = 0;i < 8;i++) 
-						if (((result >> i) & 0x1) == 1)
-							parity = ~parity;
-					cpu.PF = parity;
-
-					print_asm("test %%%s,%%%s", regsw[left_code] , regsw[right_code]);
-					return 2;
-				}
-		default:
-				assert (0);
-				return 1;
+	int result;
+	if (modrm.mod == 3) {
+		int right_code = modrm.R_M;
+		int left_code = modrm.reg;
+		result = reg_w(left_code) & reg_w(right_code);
+		print_asm("test %%%s,%%%s", regsw[left_code] , regsw[right_code]);
 	}
+	else {
+		swaddr_t addr;
+		read_ModR_M(eip + 1, &addr);
+		int left_code = modrm.reg;
+		int right = swaddr_read(addr, 4);
+		result = right & reg_w(left_code);
+		print_asm("test %%%s,0x%x", regsw[left_code] , addr);
+	}
+	cpu.OF = 0;
+	cpu.CF = 0;
+	cpu.SF = (result >> 31) & 0x1;
+	cpu.ZF = !!result;
+	bool parity = 1;
+	int i;
+	for (i = 0;i < 8;i++) 
+		if (((result >> i) & 0x1) == 1)
+			parity = ~parity;
+	cpu.PF = parity;
+	return 2;
+}
+make_helper(test_rm_r_b) {
+	ModR_M modrm;
+	modrm.val = instr_fetch(eip+1,1);
+	int result;
+	if (modrm.mod == 3) {
+		int right_code = modrm.R_M;
+		int left_code = modrm.reg;
+		result = reg_b(left_code) & reg_b(right_code);
+		print_asm("test %%%s,%%%s", regsb[left_code] , regsb[right_code]);
+	}
+	else {
+		swaddr_t addr;
+		read_ModR_M(eip + 1, &addr);
+		int left_code = modrm.reg;
+		int right = swaddr_read(addr, 4);
+		result = right & reg_b(left_code);
+		print_asm("test %%%s,0x%x", regsb[left_code] , addr);
+	}
+	cpu.OF = 0;
+	cpu.CF = 0;
+	cpu.SF = (result >> 31) & 0x1;
+	cpu.ZF = !!result;
+	bool parity = 1;
+	int i;
+	for (i = 0;i < 8;i++) 
+		if (((result >> i) & 0x1) == 1)
+			parity = ~parity;
+	cpu.PF = parity;
+	return 2;
 }
 
 
--- src/exec/logic-ins/test.h
+++ src/exec/logic-ins/test.h
@@ -2,6 +2,7 @@
 #define __TEST_H__
 
 make_helper(test_rm_r_v);
+make_helper(test_rm_r_b);
 
 
 
