324e32c5cc6e8d073d8381c53bb44ced6f27d679
Gang Jin <smartwsw@gmail.com>
2014-11-03 11:24:53 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  11:24:53 up 1 day, 12:29,  6 users,  load average: 0.02, 0.02, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---vim      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/bin-ins/grp1-template.h
+++ src/exec/bin-ins/grp1-template.h
@@ -7,7 +7,7 @@ make_helper(concat(grp1_, SUFFIX)) {
 	int opcode = instr_fetch(eip, 1); 
 	m.val = instr_fetch(eip + 1,1);
 	int len = 1, result;
-	int imm;
+	int imm, value;
 	swaddr_t addr;
 	if (opcode == 0x83) {
 		imm = instr_fetch(eip + 2,1);
@@ -18,19 +18,17 @@ make_helper(concat(grp1_, SUFFIX)) {
 		imm = instr_fetch(eip + 2, DATA_BYTE);
 		len += DATA_BYTE + 1;
 	}   
-	if (m.mod == 3) {
-		int reg_value = REG(m.reg);
-		if (reg_value < imm) 
-			cpu.CF = 1;
-		result = reg_value - imm;
-	}   
+	if (m.mod == 3) 
+		value = REG(m.reg);
 	else {
 		len += read_ModR_M(eip + 1, &addr);
-		int m_val = swaddr_read(addr, 1); 
-		result = m_val - imm;
+		value = swaddr_read(addr, 1); 
 	}   
 	switch (m.opcode) {
 		case 7 : { 
+					 result = value - imm;
+					 if (value < imm)
+						 cpu.CF = 1;
 					 if (m.mod == 3)
 						 print_asm("cmp"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
 					 else
@@ -38,6 +36,9 @@ make_helper(concat(grp1_, SUFFIX)) {
 					 break;
 				 }   
 		case 5 : { 
+					 result = value - imm;
+					 if (value < imm)
+						 cpu.CF = 1;
 					 if (m.mod == 3) {
 						 REG(m.reg) = result;
 						 print_asm("sub"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
@@ -48,11 +49,37 @@ make_helper(concat(grp1_, SUFFIX)) {
 					 }
 					 break;
 				 }
+		case 0: {
+					result = value + imm;
+					cpu.CF = ((value >> 31) & 0x1) && ((imm >> 31) & 0x1); 
+					if (m.mod == 3) {
+						REG(m.reg) = result;
+						print_asm("add"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
+					}
+					else {
+						swaddr_write(addr, DATA_BYTE, result);
+						print_asm("add"str(SUFFIX)"\t\t$0x%x,0x%x",imm, addr);
+					}
+					break;
+				}
+		case 2: {
+					result = value + imm + cpu.CF;
+					cpu.CF = ((value >> 31) & 0x1) && ((imm >> 31) & 0x1); 
+					if (m.mod == 3) {
+						REG(m.reg) = result;
+						print_asm("add"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(m.reg));
+					}
+					else {
+						swaddr_write(addr, DATA_BYTE, result);
+						print_asm("add"str(SUFFIX)"\t\t$0x%x,0x%x",imm, addr);
+					}
+					break;
+				}
+
 		default :
-				 assert(0);
+				assert(0);
 	}
-	cpu.AF = 0;
-	cpu.ZF = !!result;
+	cpu.ZF = !result;
 	cpu.SF = (result >> 31) & 0x1;
 	cpu.OF = 0;
 	bool parity = 1;
