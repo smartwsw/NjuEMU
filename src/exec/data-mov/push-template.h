9dcac578184d0070c1ff322741e5c3d721177936
Gang Jin <smartwsw@gmail.com>
2014-11-05 17:22:58 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  17:22:58 up 2 days,  9:40,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---make---sh-+-memu      |                                       `-tee      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/data-mov/push-template.h
+++ src/exec/data-mov/push-template.h
@@ -2,6 +2,8 @@
 #include "exec/template-start.h"
 #include "cpu/modrm.h"
 
+extern char suffix;
+
 make_helper(concat(push_r_, SUFFIX)) {
 	int reg_code = instr_fetch(eip,1) & 0x7;
 	int value = REG(reg_code);
@@ -23,6 +25,20 @@ make_helper(concat(push_m_, SUFFIX)) {
 					 print_asm("push"str(SUFFIX)"\t\t0x%x", addr);
 					 return len + 1;
 				 }
+		case 4: {
+					if (m.mod == 3) {
+						cpu.eip = (suffix == 'l') ? (REG(m.R_M) - 2) : ((REG(m.R_M) - 2) & 0xffff);
+						print_asm("jmp\t\t%%%s", REG_NAME(m.R_M));
+						return 2;
+					}
+					else {
+						swaddr_t addr;
+						int len = read_ModR_M(eip + 1, &addr) + 1;
+						cpu.eip = (suffix == 'l') ? (MEM_R(addr) - 2) : ((MEM_R(addr) - 2) & 0xffff);
+						print_asm("jmp\t\t%s", ModR_M_asm);
+						return len;
+					}
+				}
 		default :
 				 assert(0);
 	}
