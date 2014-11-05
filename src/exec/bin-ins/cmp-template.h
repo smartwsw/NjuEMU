25c2b687ff5b094a99b20fc4480719de55fb1200
Gang Jin <smartwsw@gmail.com>
2014-11-05 19:33:33 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  19:33:33 up 2 days, 11:50,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash]      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/bin-ins/cmp-template.h
+++ src/exec/bin-ins/cmp-template.h
@@ -3,14 +3,19 @@
 #include "cpu/modrm.h"
 make_helper(concat(cmp_i_a_,SUFFIX)) {
 	int imm = instr_fetch(eip + 2, DATA_BYTE);
+	int opcode = instr_fetch(eip, 1);
+	if(opcode == 0x1c || opcode == 0x1d)
+		imm += cpu.CF;
 	int result = REG(0) - imm;
-	cpu.AF = 0;
 	cpu.ZF = !result;
 	cpu.SF = (result >> 31) & 0x1;
-	cpu.OF = 0;
+	cpu.OF = (MSB(concat(reg_, SUFFIX)(0)) != MSB(imm)) && (MSB(imm) == MSB(result));
 	if(instr_fetch(eip, 1) == 0x2c || instr_fetch(eip, 1) == 0x2d) {
 		REG(0) = result;
-		print_asm("sub"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(0));
+		if (opcode == 0x1c || opcode == 0x1d)
+			print_asm("sbb"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(0));
+		else 
+			print_asm("sub"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(0));
 	}
 	else 
 		print_asm("cmp"str(SUFFIX)"\t\t$0x%x,%%%s",imm,REG_NAME(0));
@@ -23,24 +28,34 @@ make_helper(concat(cmp_rm_r_, SUFFIX)) {
 	if(m.mod == 3) {
 		int reg = REG(m.reg);
 		int r_m = REG(m.R_M);
-		if (opcode == 0x38 || opcode == 0x39 || opcode == 0x28 || opcode == 0x29) {
+		if (opcode == 0x38 || opcode == 0x39 || opcode == 0x28 || opcode == 0x29 || opcode == 0x18 || opcode == 0x19) {
+			if (opcode == 0x19 || opcode == 0x18)
+				reg += cpu.CF;
 			result = r_m - reg;
 			cpu.CF = (r_m < reg);
 			cpu.OF = (MSB(r_m) != MSB(reg)) && (MSB(reg) == MSB(result));
-			if (opcode == 0x28 || opcode == 0x29) {
+			if (opcode == 0x28 || opcode == 0x29 || opcode == 0x19 || opcode == 0x18) {
 				REG(m.R_M) = result;
-				print_asm("sub"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.reg),REG_NAME(m.R_M));
+				if (opcode == 0x18 || opcode == 0x19)
+					print_asm("sbb"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.reg),REG_NAME(m.R_M));
+				else
+					print_asm("sub"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.reg),REG_NAME(m.R_M));
 			}
 			else
 				print_asm("cmp"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.reg),REG_NAME(m.R_M));
 		}
 		else {
+			if (opcode == 0x1a || opcode == 0x1b)
+				reg += cpu.CF;
 			result = reg - r_m;
 			cpu.CF = (reg < r_m);
 			cpu.OF = (MSB(r_m) != MSB(reg)) && (MSB(r_m) == MSB(result));
-			if (opcode == 0x2A || opcode == 0x2B) {
+			if (opcode == 0x2A || opcode == 0x2B || opcode == 0x1a || opcode == 0x1b) {
 				REG(m.reg) = result;
-				print_asm("sub"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.R_M),REG_NAME(m.reg));
+				if (opcode == 0x1a || opcode == 0x1b)
+					print_asm("sbb"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.R_M),REG_NAME(m.reg));
+				else 
+					print_asm("sub"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.R_M),REG_NAME(m.reg));
 			}
 			else 
 				print_asm("cmp"str(SUFFIX)"\t\t%%%s,%%%s",REG_NAME(m.R_M),REG_NAME(m.reg));
