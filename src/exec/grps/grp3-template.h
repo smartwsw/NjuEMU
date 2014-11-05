eae131ecd7fcbfe528138ffe983bb73c8588f47a
Gang Jin <smartwsw@gmail.com>
2014-11-05 22:56:19 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  22:56:19 up 2 days, 14:28,  4 users,  load average: 0.08, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash---vi]      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/grps/grp3-template.h
+++ src/exec/grps/grp3-template.h
@@ -9,6 +9,8 @@ make_helper(concat(grp3_, SUFFIX)) {
 	DATA_TYPE imm;
 	switch (m.opcode) {
 		case 0 : {
+					 cpu.OF = 0;
+					 cpu.SF = 0;
 					 if (m.mod == 3) {
 						 int reg = REG(m.R_M);
 						 imm = instr_fetch(eip + 1, DATA_BYTE);
@@ -40,14 +42,32 @@ make_helper(concat(grp3_, SUFFIX)) {
 						swaddr_write(addr, DATA_BYTE, ~tmp);
 						print_asm("not"str(SUFFIX)"\t\t%s", ModR_M_asm);
 					}
+					return len;
 				}
-
+		case 3: {
+					if (m.mod == 3) {
+						 cpu.CF = !!REG(m.R_M);
+						 result = -REG(m.R_M);
+						 cpu.OF = MSB(result) ^ MSB(REG(m.R_M));
+						 REG(m.R_M) = result;
+						 print_asm("neg\t\t%%%s",REG_NAME(m.R_M));
+						 len++;
+					}
+					else {
+						swaddr_t addr;
+						len += read_ModR_M(eip + 1, &addr);
+						result = -MEM_R(addr);
+						cpu.CF = !!MEM_R(addr);
+						cpu.OF = MSB(result) ^ MSB(MEM_R(addr));
+						MEM_W(addr, result);
+						print_asm("neg\t\t%s", ModR_M_asm);
+					}
+					return len;
+				}
 		default :
 				assert(0);
 
 	}
-	cpu.OF = 0;
-	cpu.CF = 0;
 	cpu.SF = (result >> 31) & 0x1;
 	cpu.ZF = !result;
 	bool parity = 1;
