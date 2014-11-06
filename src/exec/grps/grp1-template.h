1c5d4b28308b858d491ef34e08c1b22d0464b933
Gang Jin <smartwsw@gmail.com>
2014-11-06 16:28:02 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  16:28:02 up 2 days, 15:18,  4 users,  load average: 0.12, 0.04, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash---vi]      |-udevd---2*[udevd]      |-udisks-daemon-+-udisks-daemon      |               `-{udisks-daemon}      |-upowerd---2*[{upowerd}]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/grps/grp1-template.h
+++ src/exec/grps/grp1-template.h
@@ -18,7 +18,7 @@ make_helper(concat(grp1_, SUFFIX)) {
 	}   
 	if (opcode == 0x83) {
 		imm = instr_fetch(eip + len + 1, 1);
-	//	imm = (imm << 24) >>24;
+		imm = (imm << 24) >>24;
 		len += 2;
 	}   
 	else {
