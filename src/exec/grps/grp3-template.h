af7a74e7a833023e89ce5d39274c85446ed7624a
Gang Jin <smartwsw@gmail.com>
2014-11-07 19:51:42 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  19:51:42 up  1:39,  4 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---bash---make---sh-+-git      |                                              `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/grps/grp3-template.h
+++ src/exec/grps/grp3-template.h
@@ -6,7 +6,7 @@ make_helper(concat(grp3_, SUFFIX)) {
 	ModR_M m;
 	m.val = instr_fetch(eip+1,1);
 	int result, len = 1;
-	DATA_TYPE imm;
+	DATA_TYPE_S imm;
 	switch (m.opcode) {
 		case 0 : {
 					 cpu.OF = 0;
@@ -46,12 +46,12 @@ make_helper(concat(grp3_, SUFFIX)) {
 				}
 		case 3: {
 					if (m.mod == 3) {
-						 cpu.CF = !!REG(m.R_M);
-						 result = -REG(m.R_M);
-						 cpu.OF = MSB(result) ^ MSB(REG(m.R_M));
-						 REG(m.R_M) = result;
-						 print_asm("neg\t\t%%%s",REG_NAME(m.R_M));
-						 len++;
+						cpu.CF = !!REG(m.R_M);
+						result = -REG(m.R_M);
+						cpu.OF = MSB(result) ^ MSB(REG(m.R_M));
+						REG(m.R_M) = result;
+						print_asm("neg\t\t%%%s",REG_NAME(m.R_M));
+						len++;
 					}
 					else {
 						swaddr_t addr;
@@ -63,6 +63,48 @@ make_helper(concat(grp3_, SUFFIX)) {
 						print_asm("neg\t\t%s", ModR_M_asm);
 					}
 					return len;
+				}
+		case 4: {
+					uint64_t rst;
+					if(m.mod == 3) {
+						rst = (uint64_t)REG(0) * (uint64_t)REG(m.R_M);
+						REG(0) = rst;
+						cpu.CF = !!(rst & 0xffffffff00000000);
+						cpu.OF = cpu.CF;
+						print_asm("mul\t\t%%%s", REG_NAME(m.R_M));
+						return len + 1;
+					}
+					else {
+						swaddr_t addr;
+						len += read_ModR_M(eip + 1, &addr);
+						rst = (uint64_t)REG(0) * (uint64_t)MEM_R(addr);
+						REG(0) = rst;
+						cpu.CF = !!(rst & 0xffffffff00000000);
+						cpu.OF = cpu.CF;
+						print_asm("mul\t\t%s", ModR_M_asm);
+						return len;
+					}
+				}
+		case 5: {
+					int64_t rst;
+					if(m.mod == 3) {
+						rst = (int64_t)(DATA_TYPE_S)REG(0) * (int64_t)(DATA_TYPE_S)REG(m.R_M);
+						REG(0) = rst;
+						cpu.CF = !!(rst & 0xffffffff00000000);
+						cpu.OF = cpu.CF;
+						print_asm("imul\t\t%%%s", REG_NAME(m.R_M));
+						return len + 1;
+					}
+					else {
+						swaddr_t addr;
+						len += read_ModR_M(eip + 1, &addr);
+						rst = (int64_t)(DATA_TYPE_S)REG(0) * (int64_t)(DATA_TYPE_S)MEM_R(addr);
+						REG(0) = rst;
+						cpu.CF = !!(rst & 0xffffffff00000000);
+						cpu.OF = cpu.CF;
+						print_asm("imul\t\t%s", ModR_M_asm);
+						return len;
+					}
 				}
 		default :
 				assert(0);
