5cee279519ba95d94c5035697d6a953442a9ba57
Gang Jin <smartwsw@gmail.com>
2014-11-08 18:41:52 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  18:41:52 up  8:00,  5 users,  load average: 0.05, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      |-sshd---sshd---bash---make---sh-+-memu      |      |                                `-tee      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---vi      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/string/rep.c
+++ src/exec/string/rep.c
@@ -7,12 +7,9 @@ extern int exec(swaddr_t);
 make_helper(rep) {
 	int len = 0;
 	while (reg_l(R_ECX) > 0) {
-		printf("test0   %x\n", eip);
 		len = exec(eip + 1);
-		printf("test1\n");
 		reg_l(R_ECX)--;
 	}
-	printf("%x\n", cpu.eip);
 	switch (swaddr_read(cpu.eip + 1, 1)) {
 		case 0xa4:
 			print_asm("rep movsb");
