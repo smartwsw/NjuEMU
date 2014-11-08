27ea28f3add1f068e78c9e6c01b22f2d798e099a
Gang Jin <smartwsw@gmail.com>
2014-11-08 21:10:43 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  21:10:43 up 10:29,  5 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -16,23 +16,7 @@ void loader() {
 	Elf32_Phdr *ph = (void *)elf->e_phoff;
 	int i = 0;
 	for(; i < elf->e_phnum; i ++) {
-		if(ph[i].p_type == PT_LOAD) {
-			int j;
-			void *dst;
-			const void* src;
-			dst = (void*)ph[i].p_vaddr;
-			src = (void*)(elf + ph[i].p_offset);
-			for (j = 0; j < ph[i].p_filesz; j++) {
-				*(char*)dst = *(char*)src;
-				dst = (char*)dst + 1;
-				src = (char*)src + 1;
-			}
-			for (j = 0; j < ph[i].p_memsz - ph[i].p_filesz; j++) {
-				*(char*)dst = 0;
-				dst = (char*)dst + 1;
-			}
-
-		}
+		memcpy((void *)ph[i].p_vaddr, elf + ph[i].p_offset, ph[i].p_filesz);
 	}
 
 	((void(*)(void)) elf->e_entry)();
