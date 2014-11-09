5065f4367066afb797debc344fc5444024258c8a
Gang Jin <smartwsw@gmail.com>
2014-11-09 12:33:27 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  12:33:27 up 14:24,  6 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-nemu      |      |                                `-tee      |      |-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -1,16 +1,6 @@
 #include <elf.h>
 #include "trap.h"
 
-void memcpy(void *dest, void *src, int size) {
-	asm volatile("cld");
-	asm volatile("rep movsl" : : "D"(dest), "S"(src), "c"(size));
-}
-
-void memset(void *dest, int value, int size) {
-	asm volatile("cld");
-	asm volatile("rep stosl" : : "D"(dest), "a"(value), "c"(size));
-}
-
 void loader() {
 	Elf32_Ehdr *elf = (void *)0;
 	Elf32_Phdr *ph = (void *)elf->e_phoff;
@@ -19,7 +9,7 @@ void loader() {
 		if (ph[i].p_type == PT_LOAD) {
 			int j;
 			void* dst;
-			const void* src;
+			void* src;
 			dst = (void*)ph[i].p_vaddr;
 			src = (void*)(elf + ph[i].p_offset);
 			for (j = 0; j < ph[i].p_filesz; j++) {
@@ -30,16 +20,6 @@ void loader() {
 			for (j=0; j < ph[i].p_memsz - ph[i].p_filesz; j++) {
 				*(char*)dst = 0;
 				dst = (char*)dst + 1;
-			}
-			int tmp = ph[i].p_memsz % 4;
-			if (!tmp)
-				continue;
-			else {
-				int j;
-				for (j = 0; j < 4 - tmp ; j++) {
-					*(char*)dst = 0;
-					dst = (char*)dst + 1;
-				}
 			}
 		}
 	}
