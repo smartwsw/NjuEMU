a0a32bb474cdb14ed9edf9b4599a58b692548b2a
Gang Jin <smartwsw@gmail.com>
2014-11-09 14:39:31 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  14:39:31 up 16:30,  7 users,  load average: 0.01, 0.04, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---bash]      |      |-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -13,9 +13,7 @@ void loader() {
 			const void* src;
 			dst = (void*)ph[i].p_vaddr;
 			src = (void*)ph[i].p_offset;
-					//nemu_assert(src == (void *)ph[i].p_offset);
 			for (j = 0; j < ph[i].p_filesz; j++) {
-					nemu_assert(src == (void *)ph[i].p_offset);
 				*(char*)dst = *(char*)src;
 				dst = (char*)dst + 1;
 				src = (char*)src + 1;
