ce29f0211b5bfbd33cde4d7524c274f5a253ec80
Gang Jin <smartwsw@gmail.com>
2014-11-10 08:03:28 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2 i686 GNU/Linux  08:03:28 up 18:22,  2 users,  load average: 0.00, 0.01, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/exec/ctrl-trans/ret.c
+++ src/exec/ctrl-trans/ret.c
@@ -3,6 +3,7 @@
 extern char suffix;
 
 make_helper(ret) {
+	int opcode = instr_fetch(eip, 1);
 	if(suffix == 'l') {
 		cpu.eip = swaddr_read(reg_l(R_ESP), 4) - 1;
 		reg_l(R_ESP) += 4;
@@ -11,6 +12,9 @@ make_helper(ret) {
 		cpu.eip = swaddr_read(reg_l(R_ESP), 2) - 1;
 		reg_l(R_ESP) += 2;
 	}
+	if (opcode == 0xc2)
+		cpu.esp += instr_fetch(eip + 1, 2);
 	print_asm("ret");
 	return 1;
 }
+
