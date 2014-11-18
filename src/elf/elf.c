babc90d5339439c3f2d0287d21cac8db75a727ac
Gang Jin <smartwsw@gmail.com>
2014-11-18 16:54:33 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2+deb7u1 i686 GNU/Linux  16:54:33 up  1:29,  5 users,  load average: 0.05, 0.07, 0.06 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---gdb---fib      |-udevd---2*[udevd]      |-vmtoolsd      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/elf/elf.c
+++ src/elf/elf.c
@@ -122,7 +122,7 @@ char* find_name(swaddr_t addr) {
 			printf("%d %d\n", symtab[i].st_value, symtab[i].st_size);
 				if (addr >= symtab[i].st_value && addr <= symtab[i].st_value + symtab[i].st_size) {
 					char *name = NULL;
-					strcpy(strtab + symtab[i].st_name, name);
+					//strcpy(strtab + symtab[i].st_name, name);
 					printf("%s\n%s\n", name, strtab + symtab[i].st_name);
 					return name;
 				}
