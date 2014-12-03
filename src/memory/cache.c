bb6e7177166603d58667b38ea0e4e409e11188a5
Gang Jin <smartwsw@gmail.com>
2014-12-03 14:57:50 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2+deb7u1 i686 GNU/Linux  14:57:50 up 16:40,  2 users,  load average: 0.01, 0.05, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---vi      |-tmux---2*[bash---vi]      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -19,5 +19,25 @@ typedef struct {
 	uint8_t data[64];
 } block;
 
-block l1_cache[8][128];
+block l1_cache[128][8];
 
+uint32_t l1cache_read(hwaddr_t addr, size_t len) {
+	assert(len == 1 || len == 2 || len == 4);
+	cache_addr temp;
+	temp.addr = addr;
+	int i;
+//	if (((addr / 64 * 64 ) != ((addr + len) / 64 * 64)) ** (len > 1))
+//		return dram_read(addr, len);
+	for (i = 0; i < 8 ; i++){
+		if (l1_cache[temp.index][i].valid)
+			if (l1_cache[temp.index][i].tag == temp.tag)
+				return 0;
+//				return *(uint32_t *)(l1_cache[temp.index][i].data[temp.c_addr]);
+	}
+	for (i = 0; i < 8; i++) {
+		if (!l1_cache[temp.index][i].valid) {
+			l1_cache[temp.index][i].valid = true;
+		}
+	}
+	return 0;
+}
