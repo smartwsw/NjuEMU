5a600b551560e28b025f56f56c6a6cc5b9f07f6d
Gang Jin <smartwsw@gmail.com>
2014-12-07 16:31:05 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2+deb7u1 i686 GNU/Linux  16:31:05 up 1 day,  3:06,  4 users,  load average: 0.02, 0.03, 0.05 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash---vi      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-tmux---2*[bash---vi]      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

--- include/cache.h
+++ include/cache.h
@@ -3,4 +3,6 @@
 
 #include "common.h"
 
+uint32_t L1cache_read(hwaddr_t, size_t);
+void L1cache_write(hwaddr_t, size_t, uint32_t);
 #endif
--- src/memory/memory.c
+++ src/memory/memory.c
@@ -2,17 +2,19 @@
 
 uint32_t dram_read(hwaddr_t addr, size_t len);
 void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+uint32_t L1cache_read(hwaddr_t, size_t);
+void L1cache_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
+	return L1cache_read(addr, len);
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-	dram_write(addr, len, data);
+	L1cache_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
@@ -25,12 +27,12 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 	hwaddr_write(addr, len, data);
 }
 
-static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
-	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
-}
+//static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
+//	assert(len == 1 || len == 2 || len == 4);
+//	return dram_read(addr, len);
+//}
 
 uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return hwaddr_read_instr(addr, len);
+	return swaddr_read(addr, len);
 }
