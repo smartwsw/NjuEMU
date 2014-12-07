ee9fdc9124d0a6ae4b86c826a4002891e46aab59
Gang Jin <smartwsw@gmail.com>
2014-12-07 16:22:35 +0800

    > compile Linux debian 3.2.0-4-686-pae #1 SMP Debian 3.2.63-2+deb7u1 i686 GNU/Linux  16:22:35 up 1 day,  2:58,  3 users,  load average: 0.32, 0.08, 0.07 init-+-acpid      |-console-kit-dae---64*[{console-kit-dae}]      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-polkitd---{polkitd}      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---bash---vi      |-sshd-+-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      |-tmux---2*[bash---vi]      |-udevd---2*[udevd]      |-vmtoolsd---{vmtoolsd}      `-vmware-vmblock----2*[{vmware-vmblock-}]

---
+++ src/memory/cache-template.h
@@ -0,0 +1,149 @@
+#define TAG_LEN (27 - INDEX_LEN - OFFSET_LEN)
+
+typedef union {
+	struct {
+		uint32_t offset	: OFFSET_LEN;
+		uint32_t index	: INDEX_LEN;
+		uint32_t tag	: TAG_LEN;
+	};
+	uint32_t addr;
+} concat(Cache, _addr);
+
+#define BLOCK_SIZE (1 << OFFSET_LEN)
+#define INDEX_NUM (1 << INDEX_LEN)
+
+typedef struct {
+	uint8_t block[BLOCK_SIZE];
+	uint32_t tag;
+	bool valid;
+#ifdef WRITE_BACK
+	bool dirty;
+#endif
+} Cache[INDEX_NUM][LINE_NUM];
+
+Cache cache;
+void concat(init_, cache)() {
+	int i = 0,j = 0;
+	for (; i < LINE_NUM; i++)
+		for (; j < INDEX_NUM; j++)
+			cache[i][j].valid = false;
+}
+
+#define DATA_MASK 3
+void concat(cache, _replace)(uint32_t index, int line, uint32_t tag) {
+	int k = 0;
+	concat(Cache, _addr) tmp;
+	tmp.addr = 0;
+	tmp.index = index;
+#ifdef WRITE_BACK
+	if (cache[index][line].valid && ccache[index][line].dirty) {
+		tmp.tag = cache[index][line].tag;
+		for (k = 0; k < BLOCK_SIZE; k++)
+			next_write(tmp.addr + k, 1, cache[index][line].block[k]);
+	}
+	cache[index][line].dirty = false;
+#endif
+	tmp.tag = tag;
+	for (k = 0; k < BLOCK_SIZE; k++)
+		cache[index][line].block[k] = next_read(tmp.addr + k, 1);
+	cache[index][line].tag = tag;
+	cache[index][line].valid = true;
+}
+
+void concat(cache, _hwread)(hwaddr_t addr,  void* data) {
+	concat(Cache, _addr) tmp;
+	tmp.addr = addr;
+	uint32_t offset = tmp.offset;
+	uint32_t index = tmp.index;
+	uint32_t tag = tmp.tag;
+	int i;
+	for (i = 0; i < LINE_NUM; i++) 
+		if (cache[index][i].valid && (cache[index][i].tag == tag))
+			break;
+	if (i == LINE_NUM) {
+		i = rand() % LINE_NUM;
+		concat(cache, _replace)(index, i, tag);
+	}
+	memcpy(data, cache[index][i].block + offset, 4);
+}
+
+void concat(cache, _hwwrite)(hwaddr_t addr, void* data, uint8_t *mask) {
+	concat(Cache, _addr) tmp;
+	tmp.addr = addr & (~DATA_MASK);
+	uint32_t offset = tmp.offset;
+	uint32_t index = tmp.index;
+	uint32_t tag = tmp.tag;
+	int i;
+	for (i = 0; i < LINE_NUM; i++) 
+		if (cache[index][i].valid && (cache[index][i].tag == tag))
+				break;
+	if (i < LINE_NUM)
+		memcpy_with_mask(cache[index][i].block + offset, data, 4, mask);
+#ifdef WRITE_BACK
+	if (i == LINE_NUM) {
+		i = rand() % LINE_NUM;
+		concat(cache, _replace)(index, i, tag);
+		memcpy_with_mask(cache[index][i].block + offset, data, 4, mask);
+	}
+	cache[index][i].dirty = true;
+#endif
+}
+
+uint32_t concat(cache, _read)(hwaddr_t addr, size_t len) {
+	assert ((len == 1) || (len == 2) || ( len == 4));
+	uint32_t offset = addr & DATA_MASK;
+	uint8_t tmp[8];
+	concat(cache, _hwread)(addr, tmp);
+	if ((addr ^ (addr + len - 1)) & (~DATA_MASK))
+		concat(cache, _hwread)(addr + 4, tmp + 4);
+	return *(uint32_t*)(tmp + offset) & (~0u >> ((4 - len) << 3));
+}
+void concat(cache, _write)(hwaddr_t addr, size_t len, uint32_t data) {
+	assert ((len == 1) || (len == 2) || ( len == 4));
+	uint32_t offset = addr & DATA_MASK;
+	uint8_t tmp[8];
+	uint8_t mask[8];
+	memset(mask, 0, 8);
+	*(uint32_t*)(tmp + offset) = data;
+	memset(mask + offset, 1, len);
+	concat(cache, _hwwrite)(addr, tmp, mask);
+	if ((addr ^ (addr + len + 1)) & (~DATA_MASK))
+		concat(cache, _hwwrite)(addr + 4, tmp + 4, mask + 4);
+#ifndef WRITE_BACK
+	next_write(addr, len, data);
+#endif
+}
+	
+#undef OFFSET_LEN
+#undef TAG_LEN
+#undef SET_NUM
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
--- src/memory/cache.c
+++ src/memory/cache.c
@@ -1,43 +1,16 @@
 #include "common.h"
 #include "lib/misc.h"
+#include <stdlib.h>
 
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
+
+#define OFFSET_LEN 6
 #define INDEX_LEN 7
-#define C_ADDR_LEN 6
-#define TAG_LEN (27 - INDEX_LEN - C_ADDR_LEN)
-typedef union {
-	struct {
-		uint32_t tag	: TAG_LEN;
-		uint32_t index	: INDEX_LEN;
-		uint32_t c_addr	: C_ADDR_LEN;
-	};
-	uint32_t addr;
-} cache_addr;
+#define LINE_NUM 8
+#define Cache L1Cache
+#define cache L1cache
+#define next_read dram_read
+#define next_write dram_write
+#include "cache-template.h"
 
-typedef struct {
-	bool valid;
-	uint32_t tag;
-	uint8_t data[64];
-} block;
-
-block l1_cache[128][8];
-
-uint32_t l1cache_read(hwaddr_t addr, size_t len) {
-	assert(len == 1 || len == 2 || len == 4);
-	cache_addr temp;
-	temp.addr = addr;
-	int i;
-//	if (((addr / 64 * 64 ) != ((addr + len) / 64 * 64)) ** (len > 1))
-//		return dram_read(addr, len);
-	for (i = 0; i < 8 ; i++){
-		if (l1_cache[temp.index][i].valid)
-			if (l1_cache[temp.index][i].tag == temp.tag)
-				return 0;
-//				return *(uint32_t *)(l1_cache[temp.index][i].data[temp.c_addr]);
-	}
-	for (i = 0; i < 8; i++) {
-		if (!l1_cache[temp.index][i].valid) {
-			l1_cache[temp.index][i].valid = true;
-		}
-	}
-	return 0;
-}
